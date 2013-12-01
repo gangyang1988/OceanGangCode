940b83309ff3d5dbb9a9f74d31aa30ddb8d28382
gangyang1988 <gangyang1988@gmail.com>
2013-12-01 21:30:00 +0800

    adjust format

--- DesignPattern/DesignPatternMain/DesignPatternMain/DesignPatternMain.cpp
+++ DesignPattern/DesignPatternMain/DesignPatternMain/DesignPatternMain.cpp
@@ -1,12 +1,12 @@
 // DesignPatternMain.cpp : 定义控制台应用程序的入口点。
-//
+//s
 
 #include "stdafx.h"
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-printf("Hello Design Pattern World!");
+	printf("Hello Design Pattern World!");
 	return 0;
 }
 
